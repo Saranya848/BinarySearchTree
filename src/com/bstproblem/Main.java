d6f84a00f2343ff25ab423e76e9b2cd0597e5676
SaranyaSiripurapu <Saranyasripurapu@gmail.com>
2022-03-07 00:57:14 +0530

    Inserting new nodes to tree

--- src/com/bstproblem/Main.java
+++ src/com/bstproblem/Main.java
@@ -7,6 +7,16 @@ public class Main {
 		bst.insert(56);
 		bst.insert(30);
 		bst.insert(70);
+		bst.insert(3);
+		bst.insert(11);
+		bst.insert(22);
+		bst.insert(16);
+		bst.insert(40);
+		bst.insert(60);
+		bst.insert(65);
+		bst.insert(63);
+		bst.insert(67);
+		bst.insert(95);
 		System.out.println("The inorder traversal of the given binary tree is - \n");
 		System.out.print("Inorder traversal: ");
 		bst.inorder();
